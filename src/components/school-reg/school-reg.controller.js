a5edd7ee75614c6ded7e96c395085e845d1870c6
Prem Prakash <prem1014prakash@gmail.com>
2017-02-14 00:24:22 +0530

    commited latest changes

--- src/components/school-reg/school-reg.controller.js
+++ src/components/school-reg/school-reg.controller.js
@@ -5,15 +5,30 @@
     angular.module('app.schoolReg',[])
         .controller('schoolRegController',schoolRegController);
 
-    schoolRegController.$inject=['schoolRegService'];
+    schoolRegController.$inject=['$scope','schoolRegService'];
 
-    function schoolRegController(schoolRegService) {
+    function schoolRegController($scope,schoolRegService) {
         var ctrl = this;
 
+        ctrl.onCountryChange = onCountryChange;
+        ctrl.onStateChange = onStateChange;
+        ctrl.saveSchoolReg = saveSchoolReg;
         onInit();
 
-        function onInit() {
-            ctrl.country = [];
+        function onCountryChange() {
+            getStateByCountryId();
+        }
+
+        function onStateChange() {
+            getDistrictByStateId();
+        }
+
+        function saveSchoolReg() {
+
+        }
+
+        function getAllCountry() {
+            ctrl.isSearchingCountry = true;
             schoolRegService.getCountry()
                 .then(function (response) {
                     _.forEach(response,function (data) {
@@ -25,12 +40,64 @@
                                 }
                             )
                         }
+                        ctrl.isSearchingCountry = false;
                     });
-
                 })
                 .catch(function (error) {
                     console.log('error')
+                    ctrl.isSearchingCountry = false;
+                });
+        }
+
+        function getStateByCountryId() {
+            ctrl.state = [];
+            ctrl.isSearchingState = true;
+            schoolRegService.getStateByCountryId(ctrl.selectedCountry)
+                .then(function (response) {
+                    _.forEach(response,function (data) {
+                        if(data.stateName!==null){
+                            ctrl.state.push(
+                                {
+                                    id:data._id,
+                                    name:data.stateName
+                                }
+                            )
+                        }
+                        ctrl.isSearchingState = false;
+                    });
                 })
+                .catch(function (error) {
+                    console.log('error')
+                    ctrl.isSearchingState = false;
+                });
+        }
+
+        function getDistrictByStateId() {
+            ctrl.district = [];
+            ctrl.isSearchingDistrict = true;
+            schoolRegService.getDistrictByStateId(ctrl.selectedState)
+                .then(function (response) {
+                    _.forEach(response,function (data) {
+                        if(data.districtName!==null){
+                            ctrl.district.push(
+                                {
+                                    id:data._id,
+                                    name:data.districtName
+                                }
+                            )
+                        }
+                        ctrl.isSearchingDistrict = false;
+                    });
+                })
+                .catch(function (error) {
+                    console.log('error')
+                    ctrl.isSearchingDistrict =false;
+                });
+        }
+
+        function onInit() {
+            ctrl.country = [];
+            getAllCountry();
         }
     }
 })();
--- src/components/school-reg/school-reg.service.js
+++ src/components/school-reg/school-reg.service.js
@@ -10,7 +10,9 @@
 
     function schoolRegService($http,$q,endPointService) {
         var service = {
-            getCountry:getCountry
+            getCountry:getCountry,
+            getStateByCountryId:getStateByCountryId,
+            getDistrictByStateId:getDistrictByStateId
         };
 
         return service;
@@ -23,6 +25,30 @@
                 })
                 .error(function (error) {
                    deferred.reject(error);
+                });
+            return deferred.promise;
+        }
+
+        function  getStateByCountryId(countryId) {
+            var deferred = $q.defer();
+            $http.get(endPointService.getEndPoint('state'+'/'+countryId))
+                .success(function (response) {
+                    deferred.resolve(response);
+                })
+                .error(function (error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        }
+
+        function getDistrictByStateId(stateId) {
+            var deferred = $q.defer();
+            $http.get(endPointService.getEndPoint('district'+'/'+stateId))
+                .success(function (response) {
+                    deferred.resolve(response);
+                })
+                .error(function (error) {
+                    deferred.reject(error);
                 });
             return deferred.promise;
         }
